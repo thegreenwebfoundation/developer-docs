ff3603dd2a8eebf80f2edc32479dd5acb322b74a
Fershad <itsfish@fershad.com>
2022-07-01 11:53:02 +0800

    add new shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,17 @@ module.exports = (eleventyConfig) => {
     return `<a href="https://github.com/thegreenwebfoundation/co2.js/stargazers" class="self-center"><img class="my-0 self-center" alt="GitHub stars" src="https://img.shields.io/github/stars/${cleanRepo}?color=%2365B54C&label=Github%20stars&style=for-the-badge"></a>`
   });
 
+  eleventyConfig.addNunjucksShortcode("betaCodeWarning", function (link) {
+
+    const prNumber = link.split('/').pop().trim()
+    return `<div class="alert alert-warning">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+          <span>Warning: The code below is currently experimental. <a href="${link}">Related pull request #${prNumber}</a>.</span>
+          </div>
+      </div>`
+  });
+
   eleventyConfig.addNunjucksShortcode("generateDocsPath", function (slug) {
     return slug.replace('/docs', '')
   })
