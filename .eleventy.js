f64f921b3f757dd1f748ff2a2781a41741f75994
Fershad <itsfish@fershad.com>
2022-08-29 11:47:16 +0800

    add anchor links to headings

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const metagen = require('eleventy-plugin-metagen');
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownItAttrs = require('markdown-it-attrs');
 
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
@@ -12,11 +15,30 @@ const postCssPlugins = [tailwindcss(require('./tailwind.config.js')), autoprefix
 const dev = process.env.NODE_ENV !== 'production'
 
 if (!dev) { postCssPlugins.push(cssnano())}
+const slugify = (s) => encodeURIComponent(String(s).trim().toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-'))
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({
     public: './'
   })
+
+  // Process Markdown
+  let markdownItOptions = {
+    html: true // you can include HTML tags
+}
+
+let markdownItAnchorOptions = {
+    level: 2, // minimum level header -- anchors will only be applied to h2 level headers and below but not h1
+}
+
+  eleventyConfig.setLibrary("md", markdownIt(markdownItOptions).use(markdownItAnchor, {...markdownItAnchorOptions, slugify, permalink: markdownItAnchor.permalink.linkInsideHeader({
+    symbol: `<span aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" class="" width="24" height="24" viewBox="0 0 24 24" stroke-width="3" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5" />
+    <path d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5" />
+  </svg></span>`,
+    placement: 'before'
+  })}).use(markdownItAttrs));
 
   // Plugins
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
