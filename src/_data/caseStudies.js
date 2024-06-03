0fac97abfa81ec9f6ea98f14dd96219974d911ac
fershad <27988517+fershad@users.noreply.github.com>
2024-06-03 12:38:03 +0800

    remove node-fetch

--- src/_data/caseStudies.js
+++ src/_data/caseStudies.js
@@ -1,6 +1,3 @@
-
-const fetch = require('node-fetch').default;
-
 const caseStudies = async () => {
     const co2js = fetch('https://www.thegreenwebfoundation.org/wp-json/wp/v2/posts?per_page=100&categories=43,34').then(resp => resp.json()).then(data => data.filter(item => item.categories.includes(43) && item.categories.includes(34)))
     return co2js;
