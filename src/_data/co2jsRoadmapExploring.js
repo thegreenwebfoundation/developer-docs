b0418283a9d73f65b9566902560503d1b12a6a73
fershad <27988517+fershad@users.noreply.github.com>
2024-11-04 11:43:51 +0800

    fix filtering

--- src/_data/co2jsRoadmapExploring.js
+++ src/_data/co2jsRoadmapExploring.js
@@ -1,4 +1,11 @@
+const dev = process.env.NODE_ENV !== 'production'
+
 const getGithubData = async () => {
+    const { devData } = await import('../helpers/dev/githubData.js');
+    if (dev) {
+        return devData;
+    }
+    
     const { Octokit } = await import('@octokit/core');
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
     const issues = await octokit.request('GET /repos/{owner}/{repo}/issues', {
@@ -9,7 +16,7 @@ const getGithubData = async () => {
     });
 
     // Remove any issues with the "release-planned" label
-    issues.data = issues.data.filter(issue => !issue.labels.find(label => label.name === "confirm" || label.name === "designing"));
+    issues.data = issues.data.filter(issue => !issue.labels.find(label => label.name === "confirmed" || label.name === "designing"));
 
     // Check for any issues that have a label of "funding-required" and add that to the data
     issues.data.forEach(issue => {
